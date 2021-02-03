273bf17835cd46175943c325ead4d437a0d2176e
Marta Voloshyn <mvoloshyn@agileassets.com>
2021-02-03 23:36:24 +0200

    Initial commit

---
+++ src/main/java/com/petvacay/PetVacayApplication.java
@@ -0,0 +1,13 @@
+package com.petvacay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetVacayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetVacayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petvacay/PetVacayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petvacay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetVacayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
